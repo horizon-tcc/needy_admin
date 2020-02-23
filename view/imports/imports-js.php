27768ae3f977191e80eba706d8dee321ad3009b0
Gustavo-Nunes <urameshi.gustavo668@gmail.com>
2020-02-23 18:12:58 -0300

    Criação da tela de Login e Home

---
+++ view/imports/imports-js.php
@@ -0,0 +1,4 @@
+
+    <script src="../js/jquery.js"></script>
+    <script src="../bootstrap/js/bootstrap.js"></script>
+    <script src="https://kit.fontawesome.com/af21d67170.js" crossorigin="anonymous"></script>
