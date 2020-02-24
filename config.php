c4cf58e56f523fffe9b469b73382be11f9af3f06
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-02-24 01:51:09 -0300

    fix config.php

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
 <?php
 
+# DB conf
+
+# COLOQUEM O LOGIN DE VOCÊS AQUI NOS VALORES DESSAS DUAS CONSTANTES
+define('DB_USER', 'tux');
+define('DB_PASS', '@MySQL2001');
+
 define('DB_SGDB', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'db_needy');
-define('DB_USER', 'tux');
-define('DB_PASS', '@MySQL2001');
-define('DB_DSN', 'mysql:host='.DB_HOST.';dbname='.DB_NAME);
+define('DB_DSN', DB_SGDB.':host='.DB_HOST.';dbname='.DB_NAME);
