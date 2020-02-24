8d048e8c805fd3722efcc5230e9c18d4b1c019fe
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-02-23 23:56:57 -0300

    fix details

--- view/imports/imports-css.php
+++ view/imports/imports-css.php
@@ -1,2 +1,3 @@
+<link rel="stylesheet" href="../bootstrap/css/bootstrap-reboot.min.css" />
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" />
 <link rel="stylesheet" href="../css/style.css" />
-<link rel="stylesheet" href="../bootstrap/css/bootstrap.css"/>
