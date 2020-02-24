3208662983da9821a6f4382cbfdb95ed3cd29560
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-02-24 01:24:51 -0300

    general config file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_SGDB', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'db_needy');
+define('DB_USER', 'tux');
+define('DB_PASS', '@MySQL2001');
+define('DB_DSN', 'mysql:host='.DB_HOST.';dbname='.DB_NAME);
