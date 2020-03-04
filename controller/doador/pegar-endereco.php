cd06d6719dd3159e178cf4cf1fd6a1c5f812f261
Gustavo-Nunes <urameshi.gustavo668@gmail.com>
2020-03-03 22:05:32 -0300

    validação do cpf e busca de endereços por cep

---
+++ controller/doador/pegar-endereco.php
@@ -0,0 +1,20 @@
+<?php
+define("SUCESSO", 1);
+define("ERRO", 0);
+try {
+
+    $cep = $_POST['txtCep'];
+    $cep = preg_replace("/[^0-9]/","",$cep);
+
+    $url = "http://viacep.com.br/ws/$cep/xml/";
+
+    $endereco = simplexml_load_file($url);
+    
+    echo json_encode($endereco);
+    
+} catch (Exception $ex) {
+
+    $resultado = array("status" => ERRO);
+    echo (json_encode($resultado));
+}
+?>
--- js/script.js
+++ js/script.js
@@ -2,30 +2,115 @@ $(".txtCpf").mask("000.000.000-00");
 $(".txtCep").mask("00000-000");
 $(".txtRg").mask("00.000.000-0");
 
-$("#txtCpfDoador").blur(function(e){
+$("#txtCpfDoador").blur(function (e) {
 
-    console.log(validarCpf($("#txtCpfDoador").val()));
+    if (validarCpf($("#txtCpfDoador").val())) {
+        $("#txtCpfDoador").removeClass("is-invalid");
+        $("#txtCpfDoador").addClass("is-valid");
+    }
+    else {
+        $("#txtCpfDoador").removeClass("is-valid");
+        $("#txtCpfDoador").addClass("is-invalid");
+
+    }
 
 });
 
-function validarCpf(cpf){
-
-  
-    var cpfFormatado = cpf.replace(/\./g,"");
-    cpfFormatado = cpfFormatado.replace("-","");
 
 
-    var vetCpf = cpfFormatado.split("");
-    var soma = 0;
 
-    for ( var i = 0 , m = 10 ; i < vetCpf.length - 2 ; i++, m--){
+$("#txtCep").blur(function (e) {
 
-        soma += vetCpf[i] * m;
-        
+    $.ajax({
+
+        url: "../controller/doador/pegar-endereco.php",
+        type: "post",
+        dataType: "json",
+        data: {
+
+            "txtCep": $('#txtCep').val()
+        },
+        success: function (response) {
+
+            $('#txtBairro').val(response.bairro);
+            $('#txtLogradouro').val(response.logradouro);
+
+        }
+
+
+
+    });
+
+
+});
+
+
+
+
+
+function validarCpf(cpf) {
+
+    if (cpf.length == 14) {
+
+
+
+        var cpfFormatado = cpf.replace(/\./g, "");
+        cpfFormatado = cpfFormatado.replace("-", "");
+
+
+        var vetCpf = cpfFormatado.split("");
+
+        var primeiroCaracter = vetCpf[0];
+
+        for (var i = 1; primeiroCaracter == vetCpf[i]; i++) {
+            if (i == vetCpf.length - 2) {
+                return;
+            }
+        }
+
+        var primeiraSoma = 0;
+
+        for (var i = 0, m = 10; i < vetCpf.length - 2; i++ , m--) {
+
+            primeiraSoma += vetCpf[i] * m;
+
+        }
+        var primeiroResto = (primeiraSoma * 10) % 11;
+
+        if (primeiroResto == vetCpf[vetCpf.length - 2] ||
+            (primeiroResto == 10 && vetCpf[vetCpf.length - 2] == 0)) {
+
+            var segundaSoma = 0;
+
+            for (var i = 0, m = 11; i < vetCpf.length - 1; i++ , m--) {
+
+                segundaSoma += vetCpf[i] * m;
+
+            }
+
+            segundoResto = (segundaSoma * 10) % 11;
+
+            if (segundoResto == vetCpf[vetCpf.length - 1] ||
+                (segundoResto == 10 && vetCpf[vetCpf.length - 1] == 0)) {
+
+                return true;
+            }
+
+            else {
+                return false;
+            }
+        }
+        else {
+
+            return false;
+        }
+
+
     }
+    else {
 
-    console.log(soma);
-
-    return cpfFormatado;
+        return false;
+    }
+    //374.545.510-09 
 
 }
