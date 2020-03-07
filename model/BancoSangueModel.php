052368ca4694a079368842470bd656fae4d1a17f
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-03-07 19:02:20 -0300

    limpando o meu código que alguém sujou com comentários óbvios e inúteis

--- model/BancoSangueModel.php
+++ model/BancoSangueModel.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."global.php");
+require_once(__DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "global.php");
 
 class BancoSangueModel
 {
@@ -10,40 +10,42 @@ class BancoSangueModel
     private $telefones = [];
     private $pacientes = [];
 
+
     # Getters
 
-
-    /**
-     * Get the value of id
-     */ 
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * Set the value of id
-     *
-     * @return  self
-     */ 
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * Get the value of nome
-     */ 
     public function getNome()
     {
         return $this->nome;
     }
 
-    /**
-     * Set the value of nome
-     *
-     * @return  self
-     */ 
+    public function getEndereco()
+    {
+        return $this->endereco;
+    }
+
+    public function getTelefones()
+    {
+        return $this->telefones;
+    }
+
+    public function getPacientes()
+    {
+        return $this->pacientes;
+    }
+
+
+    # Setters
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
     public function setNome($nome)
     {
         $this->nome = $nome;
@@ -51,61 +53,18 @@ class BancoSangueModel
         return $this;
     }
 
-    /**
-     * Get the value of endereco
-     */ 
-    public function getEndereco()
-    {
-        return $this->endereco;
-    }
-
-    /**
-     * Set the value of endereco
-     *
-     * @return  self
-     */ 
-
     public function setEndereco($endereco)
     {
         $this->endereco = $endereco;
     }
 
-    /**
-     * Get the value of telefones
-     */ 
-    public function getTelefones()
-    {
-        return $this->telefones;
-    }
-
-    /**
-     * Set the value of telefones
-     *
-     * @return  self
-     */ 
     public function setTelefones($telefones)
     {
         $this->telefones = $telefones;
     }
 
-
-    /**
-     * Get the value of pacientes
-     */ 
-    public function getPacientes()
-    {
-        return $this->pacientes;
-    }
-
-    /**
-     * Set the value of pacientes
-     *
-     * @return  self
-     */ 
     public function setPacientes($pacientes)
     {
         $this->pacientes = $pacientes;
-
-
     }
 }
