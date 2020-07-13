2439f7d0ee53872f1d46e7c042a505ea91ced3a2
Gustavo Nunes <urameshi.gustavo668@gmail.com>
2020-07-13 12:23:01 -0300

    Concluindo a opção de cadastrar os horários do banco de sangue

--- controller/banco-sangue/adicionar-horarios-banco-sangue.php
+++ controller/banco-sangue/adicionar-horarios-banco-sangue.php
@@ -1,44 +1,300 @@
 <?php
 
-define("CB_DOMINGO","cbDomingo");
-define("CB_SEGUNDA_FEIRA", "cbSegunda-feira");
-define("CB_TERCA_FEIRA", "cbTerça-feira");
-define("CB_QUARTA_FEIRA", "cbQuarta-feira");
-define("CB_QUINTA_FEIRA", "cbQuinta-feira");
-define("CB_SEXTA_FEIRA", "cb")
-session_start();
+// Constantes para checkboxes
+define("CB_DOMINGO", "cb1");
+define("CB_SEGUNDA_FEIRA", "cb2");
+define("CB_TERCA_FEIRA", "cb3");
+define("CB_QUARTA_FEIRA", "cb4");
+define("CB_QUINTA_FEIRA", "cb5");
+define("CB_SEXTA_FEIRA", "cb6");
+define("CB_SABADO", "cb7");
 
-if ( !isset($_SESSION['horariosSemana'])  ){
+
+// constantes para status de retorno para a view
+
+define("ERRO_AO_ADICIONAR_UM_NOVO_HORARIO", 0);
+define ("SUCESSO_AO_ADICIONAR_UM_NOVO_HORARIO", 1);
+define("PARAMETROS_PARA_ADICAO_DE_UM_NOVO_HORARIO_VAZIOS", 2);
+
+// define("ID_DOMINGO", 1);
+// define("ID_SEGUNDA_FEIRA", 2);
+// define("ID_TERCA_FEIRA", 3);
+// define("ID_QUARTA_FEIRA", 4);
+// define("ID_QUINTA_FEIRA", 5);
+// define("ID_SEXTA_FEIRA", 6);
+// define("ID_SABADO", 7);
+
+session_start();
+// unset($_SESSION['horariosSemana']);
+
+if (!isset($_SESSION['horariosSemana'])) {
 
     $_SESSION['horariosSemana'] = array();
-
 }
 
-if ( isset( $_POST['cbDomingo'] ) && !empty( $_POST['cbDomingo'] )) {
+if( isset($_POST['txtHorarioAbertura']) )
+if (isset($_POST[CB_DOMINGO]) && !empty($_POST[CB_DOMINGO])) {
 
-    $novoHorario = array( "idDia" => $_POST['cbDomingo'], 
-                          "descricaoDia" => "Domingo", 
-                          "horarioAbertura" => $_POST['txtHorarioAbertura'],
-                          "horarioFechamento" => $_POST['txtHorarioFechamento'] );
+    $horarioRepetido = false;
+    $indiceRepetido = -1;
 
-    array_push($_SESSION['horariosSemana'], $novoHorario);
+    foreach ($_SESSION['horariosSemana'] as $key => $horario) {
+
+        if ($horario["idDia"] == $_POST[CB_DOMINGO]) {
+
+            $horarioRepetido = true;
+            $indiceRepetido = $key;
+        }
+    }
+
+    if ($horarioRepetido) {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_DOMINGO],
+            "descricaoDia" => "Domingo",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        $_SESSION['horariosSemana'][$indiceRepetido] = $novoHorario;
+    } else {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_DOMINGO],
+            "descricaoDia" => "Domingo",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        array_push($_SESSION['horariosSemana'], $novoHorario);
+    }
+}
+
+if (isset($_POST[CB_SEGUNDA_FEIRA]) && !empty($_POST[CB_SEGUNDA_FEIRA])) {
+
+    $horarioRepetido = false;
+    $indiceRepetido = -1;
+
+    foreach ($_SESSION['horariosSemana'] as $key => $horario) {
+
+        if ($horario["idDia"] == $_POST[CB_SEGUNDA_FEIRA]) {
+
+            $horarioRepetido = true;
+            $indiceRepetido = $key;
+        }
+    }
+
+    if ($horarioRepetido) {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_SEGUNDA_FEIRA],
+            "descricaoDia" => "Segunda-feira",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+        
+        $_SESSION['horariosSemana'][$indiceRepetido] = $novoHorario;
+        
+    } else {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_SEGUNDA_FEIRA],
+            "descricaoDia" => "Segunda-feira",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        array_push($_SESSION['horariosSemana'], $novoHorario);
+    }
+}
+
+if (isset($_POST[CB_TERCA_FEIRA]) && !empty($_POST[CB_TERCA_FEIRA])) {
+
+    $horarioRepetido = false;
+    $indiceRepetido = -1;
+
+    foreach ($_SESSION['horariosSemana'] as $key => $horario) {
+
+        if ($horario["idDia"] == $_POST[CB_TERCA_FEIRA]) {
+
+            $horarioRepetido = true;
+            $indiceRepetido = $key;
+        }
+    }
+
+    if ($horarioRepetido) {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_TERCA_FEIRA],
+            "descricaoDia" => "Terça-feira",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        $_SESSION['horariosSemana'][$indiceRepetido] = $novoHorario;
+    } else {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_TERCA_FEIRA],
+            "descricaoDia" => "Terça-feira",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        array_push($_SESSION['horariosSemana'], $novoHorario);
+    }
 }
 
 
-if ( isset( $_POST['cbSegunda-feira'] ) && !empty( $_POST['cbSegunda-feira'] )) {
 
-    $novoHorario = array( "idDia" => $_POST['cbDomingo'], 
-                          "descricaoDia" => "Domingo", 
-                          "horarioAbertura" => $_POST['txtHorarioAbertura'],
-                          "horarioFechamento" => $_POST['txtHorarioFechamento'] );
+if (isset($_POST[CB_QUARTA_FEIRA]) && !empty($_POST[CB_QUARTA_FEIRA])) {
 
-    array_push($_SESSION['horariosSemana'], $novoHorario);
+    $horarioRepetido = false;
+    $indiceRepetido = -1;
+
+    foreach ($_SESSION['horariosSemana'] as $key => $horario) {
+
+        if ($horario["idDia"] == $_POST[CB_QUARTA_FEIRA]) {
+
+            $horarioRepetido = true;
+            $indiceRepetido = $key;
+        }
+    }
+
+    if ($horarioRepetido) {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_QUARTA_FEIRA],
+            "descricaoDia" => "Quarta-feira",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        $_SESSION['horariosSemana'][$indiceRepetido] = $novoHorario;
+    } else {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_QUARTA_FEIRA],
+            "descricaoDia" => "Quarta-feira",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        array_push($_SESSION['horariosSemana'], $novoHorario);
+    }
 }
 
-echo ($_POST['cbDomingo']);
-echo ($_POST['cbSegunda-feira']);
-echo ($_POST['cbTerça-feira']);
-echo ($_POST['cbQuarta-feira']);
-echo ($_POST['cbQuinta-feira']);
-echo ($_POST['cbSexta-feira']);
-echo ($_POST['cbSábado']);
+
+if (isset($_POST[CB_QUINTA_FEIRA]) && !empty($_POST[CB_QUINTA_FEIRA])) {
+
+    $horarioRepetido = false;
+    $indiceRepetido = -1;
+
+    foreach ($_SESSION['horariosSemana'] as $key => $horario) {
+
+        if ($horario["idDia"] == $_POST[CB_QUINTA_FEIRA]) {
+
+            $horarioRepetido = true;
+            $indiceRepetido = $key;
+        }
+    }
+
+    if ($horarioRepetido) {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_QUINTA_FEIRA],
+            "descricaoDia" => "Quinta-feira",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        $_SESSION['horariosSemana'][$indiceRepetido] = $novoHorario;
+    } else {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_QUINTA_FEIRA],
+            "descricaoDia" => "Quinta-feira",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        array_push($_SESSION['horariosSemana'], $novoHorario);
+    }
+}
+
+if (isset($_POST[CB_SEXTA_FEIRA]) && !empty($_POST[CB_SEXTA_FEIRA])) {
+
+    $horarioRepetido = false;
+    $indiceRepetido = -1;
+
+    foreach ($_SESSION['horariosSemana'] as $key => $horario) {
+
+        if ($horario["idDia"] == $_POST[CB_SEXTA_FEIRA]) {
+
+            $horarioRepetido = true;
+            $indiceRepetido = $key;
+        }
+    }
+
+    if ($horarioRepetido) {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_SEXTA_FEIRA],
+            "descricaoDia" => "Sexta-feira",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        $_SESSION['horariosSemana'][$indiceRepetido] = $novoHorario;
+    } else {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_SEXTA_FEIRA],
+            "descricaoDia" => "Sexta-feira",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        array_push($_SESSION['horariosSemana'], $novoHorario);
+    }
+}
+
+if (isset($_POST[CB_SABADO]) && !empty($_POST[CB_SABADO])) {
+
+    $horarioRepetido = false;
+    $indiceRepetido = -1;
+
+    foreach ($_SESSION['horariosSemana'] as $key => $horario) {
+
+        if ($horario["idDia"] == $_POST[CB_SABADO]) {
+
+            $horarioRepetido = true;
+            $indiceRepetido = $key;
+        }
+    }
+
+    if ($horarioRepetido) {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_SABADO],
+            "descricaoDia" => "Sábado",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        $_SESSION['horariosSemana'][$indiceRepetido] = $novoHorario;
+    } else {
+
+        $novoHorario = array(
+            "idDia" => $_POST[CB_SABADO],
+            "descricaoDia" => "Sábado",
+            "horarioAbertura" => $_POST['txtHorarioAbertura'],
+            "horarioFechamento" => $_POST['txtHorarioFechamento']
+        );
+
+        array_push($_SESSION['horariosSemana'], $novoHorario);
+    }
+}
+
+
+echo json_encode($_SESSION['horariosSemana']);
